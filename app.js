ce20e1b05dda8be1d0fe25bb8797c4a3eef5887c
Dieguidev <diegogaraycullas@gmail.com>
2024-02-08 13:23:19 -0500

    first commit: creado helpers y mensajes.js

---
+++ app.js
@@ -0,0 +1,12 @@
+require("colors");
+
+const { mostrarMenu } = require("./helpers/mensajes");
+
+console.clear();
+
+const main = () => {
+  console.log("Hola mundo");
+  mostrarMenu();
+};
+
+main();
