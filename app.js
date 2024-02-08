acee07bc73c2753a59ea66d4d23e31e1b4903991
Dieguidev <diegogaraycullas@gmail.com>
2024-02-08 17:31:50 -0500

    Dandole funcionalidad al menu para que se repita infinitamente

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const { mostrarMenu, pausa } = require("./helpers/mensajes");
 
 console.clear();
 
-const main = () => {
+const main = async () => {
   console.log("Hola mundo");
-  mostrarMenu();
+
+  let opt = "";
+
+  do {
+    opt = await mostrarMenu();
+    console.log({ opt });
+    if (opt !== "0") await pausa();
+  } while (opt != 0);
+
   //   pausa();
 };
 
